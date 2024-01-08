04d446ff942cd43b2b716b42b464c8a20ff09050
Ryan Huynh <hbtoan2910@gmail.com>
2024-01-08 15:21:34 -0500

    first commit of this app

---
+++ src/main/java/com/ryanhuynh/demo/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.ryanhuynh.demo.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/ryanhuynh/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanhuynh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
